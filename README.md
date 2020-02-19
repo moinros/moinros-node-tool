b1a9446b0c8de0e9a840130f3766a6a3843fe890
moinros <1785793169@qq.com>
2020-02-20 03:15:39 +0800

    控制台提交代码

---
+++ README.md
@@ -0,0 +1,17 @@
+## JavaScript 插件包 ## 
+> * <img src="https://www.server-file.com/backups/files/face/1582137889156-4ba07554fbfbbdd9334943f9556cc12c.jpg" width="36px" height="36px"/>
+> * #### [传送门](https://www.moinros.com) #### 
+#### 存放一些自己写的JavaScript插件 ####
+<table>
+<tr>
+    <td>插件名字(Name)</td><td>描述(Description)</td>
+</tr>
+<tr>
+    <td>DomElement</td>
+    <td>参考jQuery自定义的操作DOM元素的插件,<br>主要用于设置DOM元素的CSS样式和绑定Event事件</td>
+</tr>
+<tr>
+    <td>XMPictureCut</td>
+    <td>自定义的图片裁剪插件</td>
+</tr>
+</table>
--- com/moinros/script/DomElement.js
+++ com/moinros/script/DomElement.js
@@ -1,18 +1,15 @@
-/*
-(function() {
-    /!*
-    DOM 操作 API
-*!/
+;(function() {
+    "use strict";// 严格模式
 
-// 根据 html 代码片段创建 dom 对象
+    // 根据 html 代码片段创建 dom 对象
     function createElemByHTML(html) {
-        var div = void 0;
+        let div = void 0;
         div = document.createElement('div');
         div.innerHTML = html;
         return div.children;
     }
 
-// 是否是 DOM List
+    // 是否是 DOM List
     function isDOMList(selector) {
         if (!selector) {
             return false;
@@ -23,9 +20,18 @@
         return false;
     }
 
-// 封装 document.querySelectorAll
+    // 是否为DOM节点
+    function isDom(obj) {
+        if (typeof HTMLElement === 'object') {
+            return obj instanceof HTMLElement;
+        } else {
+            return obj && typeof obj === 'object' && obj.nodeType === 1 && typeof obj.nodeName === 'string';
+        }
+    }
+
+    // 封装 document.querySelectorAll
     function querySelectorAll(selector) {
-        var result = document.querySelectorAll(selector);
+        let result = document.querySelectorAll(selector);
         if (isDOMList(result)) {
             return result;
         } else {
@@ -34,24 +40,27 @@
     }
 
     // 记录所有的事件绑定
-    var eventList = [];
+    const eventList = [];
 
-    // 创建构造函数
+    /**
+     * 自定义{DomElement}对象构造函数<br>
+     * 内部调用了document.querySelectorAll()所以兼容属性选择器
+     * @param selector {DomElement}对象,DOM对象,属性选择器
+     * @returns {DomElement}
+     * @constructor {DomElement}
+     */
     function DomElement(selector) {
         if (!selector) {
-            return;
+            return void 0;
         }
-
         // selector 本来就是 DomElement 对象，直接返回
         if (selector instanceof DomElement) {
             return selector;
         }
-
         this.selector = selector;
-        var nodeType = selector.nodeType;
-
+        let nodeType = selector.nodeType;
         // 根据 selector 得出的结果（如 DOM，DOM List）
-        var selectorResult = [];
+        let selectorResult = [];
         if (nodeType === 9) {
             // document 节点
             selectorResult = [selector];
@@ -72,136 +81,77 @@
                 selectorResult = querySelectorAll(selector);
             }
         }
-
-        var length = selectorResult.length;
+        let length = selectorResult.length;
         if (!length) {
             // 空数组
             return this;
         }
-
         // 加入 DOM 节点
-        var i = void 0;
+        let i = void 0;
         for (i = 0; i < length; i++) {
             this[i] = selectorResult[i];
         }
         this.length = length;
     }
 
-// 修改原型
+    // 修改原型
     DomElement.prototype = {
         constructor: DomElement,
-
         // 类数组，forEach
         forEach: function forEach(fn) {
-            var i = void 0;
+            let i = void 0;
             for (i = 0; i < this.length; i++) {
-                var elem = this[i];
-                var result = fn.call(elem, elem, i);
+                let elem = this[i];
+                let result = fn.call(elem, elem, i);
                 if (result === false) {
                     break;
                 }
             }
             return this;
         },
-
-        // clone
-        clone: function clone(deep) {
-            var cloneList = [];
-            this.forEach(function(elem) {
-                cloneList.push(elem.cloneNode(!!deep));
-            });
-            return $(cloneList);
-        },
-
-        // 获取第几个元素
+        /**
+         * 获取第几个元素
+         * @param index
+         * @returns {DomElement}
+         */
         get: function get(index) {
-            var length = this.length;
+            let length = this.length;
             if (index >= length) {
                 index = index % length;
             }
-            return $(this[index]);
+            return $C(this[index]);
         },
-
-        // 第一个
-        first: function first() {
-            return this.get(0);
-        },
-
-        // 最后一个
-        last: function last() {
-            var length = this.length;
-            return this.get(length - 1);
-        },
-
-        // 绑定事件
-        on: function on(type, selector, fn) {
-            // selector 不为空，证明绑定事件要加代理
-            if (!fn) {
-                fn = selector;
-                selector = null;
+        /**
+         * 获取第几个DOM元素,无参数默认第0个元素
+         * @param index
+         * @returns {*}
+         */
+        getElement: function getElement(index) {
+            if (index == null) {
+                return this[0];
             }
-
-            // type 是否有多个
-            var types = [];
-            types = type.split(/\s+/);
-
+            let length = this.length;
+            if (index >= length) {
+                index = index % length;
+            }
+            return this[index];
+        },
+        // 设置元素ID
+        id: function id(val) {
+            if (!id) {
+                return this;
+            }
             return this.forEach(function(elem) {
-                types.forEach(function(type) {
-                    if (!type) {
-                        return;
-                    }
-
-                    // 记录下，方便后面解绑
-                    eventList.push({
-                        elem: elem,
-                        type: type,
-                        fn: fn
-                    });
-
-                    if (!selector) {
-                        // 无代理
-                        elem.addEventListener(type, fn);
-                        return;
-                    }
-
-                    // 有代理
-                    elem.addEventListener(type, function(e) {
-                        var target = e.target;
-                        if (target.matches(selector)) {
-                            fn.call(target, e);
-                        }
-                    });
-                });
+                elem.id = val;
             });
         },
-
-        // 取消事件绑定
-        off: function off(type, fn) {
-            return this.forEach(function(elem) {
-                elem.removeEventListener(type, fn);
-            });
-        },
-
-        // 获取/设置 属性
-        attr: function attr(key, val) {
-            if (val == null) {
-                // 获取值
-                return this[0].getAttribute(key);
-            } else {
-                // 设置值
-                return this.forEach(function(elem) {
-                    elem.setAttribute(key, val);
-                });
-            }
-        },
-
         // 添加 class
         addClass: function addClass(className) {
             if (!className) {
                 return this;
             }
             return this.forEach(function(elem) {
-                var arr = void 0;
+                let arr = void 0;
                 if (elem.className) {
                     // 解析当前 className 转换为数组
                     arr = elem.className.split(/\s/);
@@ -219,14 +169,13 @@
                 }
             });
         },
-
         // 删除 class
         removeClass: function removeClass(className) {
             if (!className) {
                 return this;
             }
             return this.forEach(function(elem) {
-                var arr = void 0;
+                let arr = void 0;
                 if (elem.className) {
                     // 解析当前 className 转换为数组
                     arr = elem.className.split(/\s/);
@@ -243,20 +192,31 @@
                 }
             });
         },
-
+        // 获取/设置 属性
+        attr: function attr(key, val) {
+            if (val == null) {
+                // 获取值
+                return this[0].getAttribute(key);
+            } else {
+                // 设置值
+                return this.forEach(function(elem) {
+                    elem.setAttribute(key, val);
+                });
+            }
+        },
         // 修改 css
         css: function css(key, val) {
-            var currentStyle = key + ':' + val + ';';
+            let currentStyle = key + ':' + val + ';';
             return this.forEach(function(elem) {
-                var style = (elem.getAttribute('style') || '').trim();
-                var styleArr = void 0,
+                let style = (elem.getAttribute('style') || '').trim();
+                let styleArr = void 0,
                     resultArr = [];
                 if (style) {
                     // 将 style 按照 ; 拆分为数组
                     styleArr = style.split(';');
                     styleArr.forEach(function(item) {
                         // 对每项样式，按照 : 拆分为 key 和 value
-                        var arr = item.split(':').map(function(i) {
+                        let arr = item.split(':').map(function(i) {
                             return i.trim();
                         });
                         if (arr.length === 2) {
@@ -282,88 +242,68 @@
                 }
             });
         },
-
+        // 修改元素CSS的width
+        width: function width(val) {
+            return this.css("width", val + 'px');
+        },
+        // 修改元素CSS的height
+        height: function height(val) {
+            return this.css("height", val + 'px');
+        },
+        // 修改元素CSS的top
+        top: function top(val) {
+            return this.css("top", val + 'px');
+        },
+        // 修改元素CSS的right
+        right: function right(val) {
+            return this.css("right", val + 'px');
+        },
+        // 修改元素CSS的bottom
+        bottom: function bottom(val) {
+            return this.css("bottom", val + 'px');
+        },
+        // 修改元素CSS的left
+        left: function left(val) {
+            return this.css("left", val + 'px');
+        },
         // 显示
         show: function show() {
             return this.css('display', 'block');
         },
-
         // 隐藏
         hide: function hide() {
             return this.css('display', 'none');
         },
-
-        // 获取子节点
-        children: function children() {
-            var elem = this[0];
-            if (!elem) {
-                return null;
-            }
-
-            return $(elem.children);
-        },
-
-        // 获取子节点（包括文本节点）
-        childNodes: function childNodes() {
-            var elem = this[0];
-            if (!elem) {
-                return null;
-            }
-
-            return $(elem.childNodes);
-        },
-
         // 增加子节点
-        append: function append($children) {
+        append: function append($Children) {
             return this.forEach(function(elem) {
-                $children.forEach(function(child) {
+                $Children.forEach(function(child) {
                     elem.appendChild(child);
                 });
             });
         },
-
         // 移除当前节点
         remove: function remove() {
             return this.forEach(function(elem) {
                 if (elem.remove) {
                     elem.remove();
                 } else {
-                    var parent = elem.parentElement;
+                    let parent = elem.parentElement;
                     parent && parent.removeChild(elem);
                 }
             });
         },
-
         // 是否包含某个子节点
-        isContain: function isContain($child) {
-            var elem = this[0];
-            var child = $child[0];
+        isContain: function isContain($Child) {
+            let elem = this[0];
+            let child = $Child[0];
             return elem.contains(child);
         },
-
-        // 尺寸数据
-        getSizeData: function getSizeData() {
-            var elem = this[0];
-            return elem.getBoundingClientRect(); // 可得到 bottom height left right top width 的数据
-        },
-
-        // 封装 nodeName
-        getNodeName: function getNodeName() {
-            var elem = this[0];
-            return elem.nodeName;
-        },
-
-        // 从当前元素查找
-        find: function find(selector) {
-            var elem = this[0];
-            return $(elem.querySelectorAll(selector));
-        },
-
         // 获取当前元素的 text
         text: function text(val) {
             if (!val) {
                 // 获取 text
-                var elem = this[0];
+                let elem = this[0];
                 return elem.innerHTML.replace(/<.*?>/g, function() {
                     return '';
                 });
@@ -374,64 +314,31 @@
                 });
             }
         },
-
-        // 获取 html
-        html: function html(value) {
-            var elem = this[0];
-            if (value == null) {
+        // 获取 设置html,无参数为获取
+        html: function html(val) {
+            let elem = this[0];
+            if (val == null) {
                 return elem.innerHTML;
             } else {
-                elem.innerHTML = value;
+                elem.innerHTML = val;
                 return this;
             }
         },
-
         // 获取 value
-        val: function val() {
-            var elem = this[0];
-            return elem.value.trim();
-        },
-
-        // focus
-        focus: function focus() {
+        val: function val(val) {
+            let elem = this[0];
+            if (val == null) {
+                return elem.value.trim();
+            }
             return this.forEach(function(elem) {
-                elem.focus();
+                elem.value = val;
             });
         },
-
         // parent
         parent: function parent() {
-            var elem = this[0];
-            return $(elem.parentElement);
+            let elem = this[0];
+            return $C(elem.parentElement);
         },
-
-        // parentUntil 找到符合 selector 的父节点
-        parentUntil: function parentUntil(selector, _currentElem) {
-            var results = document.querySelectorAll(selector);
-            var length = results.length;
-            if (!length) {
-                // 传入的 selector 无效
-                return null;
-            }
-
-            var elem = _currentElem || this[0];
-            if (elem.nodeName === 'BODY') {
-                return null;
-            }
-
-            var parent = elem.parentElement;
-            var i = void 0;
-            for (i = 0; i < length; i++) {
-                if (parent === results[i]) {
-                    // 找到，并返回
-                    return $(parent);
-                }
-            }
-
-            // 继续查找
-            return this.parentUntil(selector, parent);
-        },
-
         // 判断两个 elem 是否相等
         equal: function equal($elem) {
             if ($elem.nodeType === 1) {
@@ -440,29 +347,38 @@
                 return this[0] === $elem[0];
             }
         },
-
-        // 将该元素插入到某个元素前面
-        insertBefore: function insertBefore(selector) {
-            var $referenceNode = $(selector);
-            var referenceNode = $referenceNode[0];
+        // 将该元素插入到某个元素之中
+        insertTarget: function insertTarget(selector) {
+            let $referenceNode = $C(selector);
+            let referenceNode = $referenceNode[0];
             if (!referenceNode) {
                 return this;
             }
             return this.forEach(function(elem) {
-                var parent = referenceNode.parentNode;
+                referenceNode.appendChild(elem);
+            })
+        },
+        // 将该元素插入到某个元素前面
+        insertBefore: function insertBefore(selector) {
+            let $referenceNode = $C(selector);
+            let referenceNode = $referenceNode[0];
+            if (!referenceNode) {
+                return this;
+            }
+            return this.forEach(function(elem) {
+                let parent = referenceNode.parentNode;
                 parent.insertBefore(elem, referenceNode);
             });
         },
-
         // 将该元素插入到某个元素后面
         insertAfter: function insertAfter(selector) {
-            var $referenceNode = $(selector);
-            var referenceNode = $referenceNode[0];
+            let $referenceNode = $C(selector);
+            let referenceNode = $referenceNode[0];
             if (!referenceNode) {
                 return this;
             }
             return this.forEach(function(elem) {
-                var parent = referenceNode.parentNode;
+                let parent = referenceNode.parentNode;
                 if (parent.lastChild === referenceNode) {
                     // 最后一个元素
                     parent.appendChild(elem);
@@ -471,22 +387,59 @@
                     parent.insertBefore(elem, referenceNode.nextSibling);
                 }
             });
-        }
-    };
-
-// new 一个对象
-    function $(selector) {
-        return new DomElement(selector);
+        },
+        // 绑定事件
+        on: function on(type, selector, fn) {
+            // selector 不为空，证明绑定事件要加代理
+            if (!fn) {
+                fn = selector;
+                selector = null;
+            }
+            // type 是否有多个
+            let types = [];
+            types = type.split(/\s+/);
+            return this.forEach(function(elem) {
+                types.forEach(function(type) {
+                    if (!type) {
+                        return;
+                    }
+                    // 记录下，方便后面解绑
+                    eventList.push({
+                        elem: elem,
+                        type: type,
+                        fn: fn
+                    });
+                    if (!selector) {
+                        // 无代理
+                        elem.addEventListener(type, fn);
+                        return;
+                    }
+                    // 有代理
+                    elem.addEventListener(type, function(e) {
+                        let target = e.target;
+                        if (target.matches(selector)) {
+                            fn.call(target, e);
+                        }
+                    });
+                });
+            });
+        },
+        // 取消事件绑定
+        off: function off(type, fn) {
+            return this.forEach(function(elem) {
+                elem.removeEventListener(type, fn);
+            });
+        },
     }
 
-// 解绑所有事件，用于销毁编辑器
-    $.offAll = function() {
-        eventList.forEach(function(item) {
-            var elem = item.elem;
-            var type = item.type;
-            var fn = item.fn;
-            // 解绑
-            elem.removeEventListener(type, fn);
-        });
-    };
-})();*/
+    /**
+     * 构建一个{DomElement}对象
+     * @param selector
+     * @returns {DomElement}
+     */
+    function $C(selector) {
+        if (selector != null) return new DomElement(selector);
+    }
+
+    window.$C = $C;
+})();
